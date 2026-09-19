be2d1d25ebd0fd18c2009c0772b8ab7739474789
Sion Zhan <sszhan24@bu.edu>
2026-09-19 17:37:48 -0400

    edited webapps deployment path routing in wsgi

--- cs412/wsgi.py
+++ cs412/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'temp_name.settings')
+sys.path.insert(0, '/home/s/sszhan24/webapps/django')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cs412.settings')
 
 application = get_wsgi_application()
